4be2a87eddadf08f1bb49c7a732049e69b7379a4
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-10-26 17:36:23 -0300

    [ADD] date_hierarchy on LoginHistoryAdmin [FIX] grupo to get_groups

--- suapintegration/admin.py
+++ suapintegration/admin.py
@@ -5,14 +5,14 @@ from suapintegration.models import LoginHistory
 
 @register(get_user_model())
 class SuapUserAdmin(ModelAdmin):
-    list_display = ('username', 'name', 'email', 'campus', 'grupos', 'last_login')
+    list_display = ('username', 'name', 'email', 'campus', 'get_groups', 'last_login')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'campus', 'groups',)
     search_fields = ('username', 'name', 'social_name', 'email', 'scholar_email', 'academic_email')
     ordering = ('name',)
 
     readonly_fields = ['created_at', 'changed_at']
 
-    def grupos(self, instance):
+    def get_groups(self, instance):
         result = ', '.join([x.name for x in instance.groups.all()])
         return f'{result}'
 
@@ -22,3 +22,4 @@ class LoginHistoryAdmin(ModelAdmin):
     list_display = ('login_at', 'user', 'ip')
     search_fields = ('user__name', 'login_at')
     ordering = ('login_at',)
+    date_hierarchy = 'login_at'
